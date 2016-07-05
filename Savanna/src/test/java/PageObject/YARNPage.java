34506050a1d4bb99a488c595f8f5b3be0aedc65c
gunho14.choi <gunho14.choi@samsung.com>
2016-07-05 21:26:35 +0900

    주석 추가

--- Savanna/src/test/java/PageObject/YARNPage.java
+++ Savanna/src/test/java/PageObject/YARNPage.java
@@ -17,16 +17,23 @@ public class YARNPage {
 	@FindBy(how=How.CLASS_NAME, using="quick-links-dropdown")
 	WebElement pageLink;
 
-	
 	public YARNPage(WebDriver driver) {
 		this.driver = driver;
 	}
 	
+	/**
+	 * 페이지 링크를 클릭한다.
+	 * 
+	 */
 	public void pageLinkClick() throws Exception {
 		pageLink.click();
 		Thread.sleep(Sleep.OneSecond);
 	}
 	
+	/**
+	 * 페이지 링크의 메뉴 목록을 리스트로 반환한다.
+	 * 
+	 */
 	public List<WebElement> readPageLinkMenu() {
 		List<WebElement> elements = pageLink.findElement(By.className("dropdown-menu")).findElements(By.cssSelector("li"));
 		return elements;
